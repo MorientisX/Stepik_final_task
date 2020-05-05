5d4d496d62945eb2668e0a80497f21f2d9979d71
MorientisX <alexanderk.shishkin@gmail.com>
2020-05-05 18:00:03 +0300

    Добавил тесты для страницы авторизации;

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,20 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTATION_PASSWORD_REPEAT = (By.CSS_SELECTOR, "id_registration-password2")
+
+    LOGIN_URL = (By.CSS_SELECTOR, "login-redirect_url")
+
+
+    
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,31 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   
     page.open()                      
     page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()          
+    page.should_be_login_link()
+
+link_register = "http://selenium1py.pythonanywhere.com/cs/accounts/login/"
+
+def test_should_be_login_form(browser):
+    page = LoginPage(browser, link_register)
+    page.open()
+    page.should_be_login_form()
+
+def test_should_be_register_form(browser):
+    page = LoginPage(browser, link_register)
+    page.open()
+    page.should_be_register_form()
+
+def test_should_be_login_url(browser):
+    page = LoginPage(browser, link_register)
+    page.open()
+    page.should_be_login_url()
