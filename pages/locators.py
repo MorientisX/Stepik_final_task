1a81069f61617ffd4f4c9dc249eb98d6409d8f2c
MorientisX <alexanderk.shishkin@gmail.com>
2020-05-08 17:57:30 +0300

    Добавил тест страницы ProductPage
    Созданы:

--- pages/locators.py
+++ pages/locators.py
@@ -18,4 +18,12 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "login-redirect_url")
 
 
+class ProductPageLocators():
+
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form>.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
     
